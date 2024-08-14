cc77354d5b786268dc8fbf4b003a4e179010d7e5
foxbit19 <foxbi19@users.noreply.github.com>
2024-08-14 17:10:18 +0200

    Fix typos

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            This is my project. I hope you find them useful!
+            This is my projects. I hope you find them useful!
           </p>
         </div>
         <div className="container py-12">
